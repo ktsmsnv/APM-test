b74c91e5e7147fe963bc13f6ae2bb3915cea6a9d
Katherine Samsonova <ktsmsnv@yandex.ru>
2024-02-29 14:38:57 +0500

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ru">
 
 <head>
     <meta charset="utf-8">
@@ -14,24 +14,16 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
-    <!-- Ваш текущий код -->
-    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
     @vite(['resources/sass/app.scss'])
 
-    {{-- <!-- Используйте base_url() для генерации абсолютного URL -->
-    <link rel="stylesheet" href="{{ asset('storage/sass/app.scss') }}">
-
-    <!-- Или для скриптов -->
-    <script src="{{ asset('storage/js/app.js') }}"></script> --}}
 
     <!-- Подключение скриптов DataTables и их плагинов -->
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
 
     <link href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css" rel="stylesheet">
@@ -40,6 +32,17 @@
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/rowreorder/1.4.1/js/dataTables.rowReorder.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+
+
+    <!-- Подключаем скрипты Bootstrap Table -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-table@1.22.2/dist/bootstrap-table.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin@1.28.0/tableExport.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-table@1.22.2/dist/bootstrap-table.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-table@1.22.2/dist/extensions/export/bootstrap-table-export.min.js"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-table@1.22.3/dist/bootstrap-table-locale-all.min.js"></script>
+
 
     {{-- иконки --}}
     <link href="{{ asset('css/fontawesome.min.css') }}" rel="stylesheet">
@@ -57,74 +60,74 @@
 </head>
 
 <body>
-    <div id="app">
-        {{-- Header --}}
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <div class="logo__pages d-flex gap-4">
-                    <img src="{{ asset('/storage/apm_apm.png') }}" alt="лого вход">
-                    {{-- <a class="navbar-brand" href="{{ url('/home') }}">
-                        {{ config('app.name', 'APM') }}
-                    </a> --}}
-                </div>
-                <div class="headerMenu d-flex align-items-center gap-5">
-                    <ul class="nav nav-pills">
-                        <li class="nav-item">
-                            <a href="{{ route('home') }}"
-                                class="nav-link @if (Request::is('home')) active @endif">Главная</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('baseRisks') }}"
-                                class="nav-link @if (Request::is('base-risks')) active @endif">Реестр рисков и
-                                возможностей</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('rco') }}"
-                                class="nav-link @if (Request::is('register-commercial-offers')) active @endif">Реестр КП</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('project-maps') }}"
-                                class="nav-link @if (Request::is('project-maps/all')) active @endif">Карты проекта</a>
-                        </li>
-                    </ul>
-                    <div class="d-flex align-items-center gap-4">
-                        @guest
-                            {{-- unauthorized --}}
-                            @if (Route::has('login'))
-                                <div class="LogInBtn">
-                                    <a type="button" class="btn btn-primary"
-                                        href="{{ route('login') }}">{{ __('Вход') }}</a>
-                                    </li>
-                            @endif
-                            {{-- authorized  --}}
-                        @else
-                            <div class="userName">
-                                <a>
-                                    {{ Auth::user()->name }}
-                                </a>
-                            </div>
-                            <div class="LogOutBtn">
-                                <a type="button" class="btn btn btn-secondary btn-rounded" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
+<div id="app">
+    {{-- Header --}}
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <div class="logo__pages d-flex gap-4">
+                <img src="{{ asset('/storage/apm_apm.png') }}" alt="лого вход">
+                {{-- <a class="navbar-brand" href="{{ url('/home') }}">
+                    {{ config('app.name', 'APM') }}
+                </a> --}}
+            </div>
+            <div class="headerMenu d-flex align-items-center gap-5">
+                <ul class="nav nav-pills">
+                    <li class="nav-item">
+                        <a href="{{ route('home') }}"
+                           class="nav-link @if (Request::is('home')) active @endif">Главная</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('baseRisks') }}"
+                           class="nav-link @if (Request::is('base-risks')) active @endif">Реестр рисков и
+                            возможностей</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('rco') }}"
+                           class="nav-link @if (Request::is('register-commercial-offers')) active @endif">Реестр КП</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('project-maps') }}"
+                           class="nav-link @if (Request::is('project-maps/all')) active @endif">Карты проекта</a>
+                    </li>
+                </ul>
+                <div class="d-flex align-items-center gap-4">
+                    @guest
+                        {{-- unauthorized --}}
+                        @if (Route::has('login'))
+                            <div class="LogInBtn">
+                                <a type="button" class="btn btn-primary"
+                                   href="{{ route('login') }}">{{ __('Вход') }}</a>
+                                </li>
+                                @endif
+                                {{-- authorized  --}}
+                                @else
+                                    <div class="userName">
+                                        <a>
+                                            {{ Auth::user()->name }}
+                                        </a>
+                                    </div>
+                                    <div class="LogOutBtn">
+                                        <a type="button" class="btn btn btn-secondary btn-rounded" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                                    {{ __('Выход') }}
-                                </a>
+                                            {{ __('Выход') }}
+                                        </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                @endguest
                             </div>
-                        @endguest
-                    </div>
                 </div>
             </div>
-        </nav>
+    </nav>
 
-        {{-- Content --}}
-        <main class="mainPage">
-            @yield('content')
-        </main>
-    </div>
+    {{-- Content --}}
+    <main class="mainPage">
+        @yield('content')
+    </main>
+</div>
 </body>
 
 </html>
