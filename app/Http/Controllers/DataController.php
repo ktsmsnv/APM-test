875c2300a729b3067ecfdecf4912fbf555bd7762
Katherine Samsonova <ktsmsnv@yandex.ru>
2024-02-29 14:36:27 +0500

    ok

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -20,13 +20,33 @@ class DataController extends Controller
         return view('home', compact('RegSInteg', 'RegEOB', 'RegNHRS', 'RegOther'));
     }
 
-    public function govno()
+    public function getData_group_1()
     {
         $RegSInteg = RegSInteg::all();
+
+        // Возвращаем данные в формате JSON
+        return response()->json($RegSInteg);
+    }
+    public function getData_group_2()
+    {
         $RegEOB = RegEOB::all();
+
+        // Возвращаем данные в формате JSON
+        return response()->json($RegEOB);
+    }
+    public function getData_group_3()
+    {
         $RegNHRS = RegNHRS::all();
+
+        // Возвращаем данные в формате JSON
+        return response()->json($RegNHRS);
+    }
+    public function getData_group_4()
+    {
         $RegOther = RegOther::all();
 
-        return view('govno', compact('RegSInteg', 'RegEOB', 'RegNHRS', 'RegOther'));
+        // Возвращаем данные в формате JSON
+        return response()->json($RegOther);
     }
+
 }
